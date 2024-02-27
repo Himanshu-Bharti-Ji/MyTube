09bde0b6056ab098d315416268c88b25de557047
Himasnhu <himanshubhartimax@gmail.com>
2024-02-28 00:55:31 +0530

    add subscription model

--- src/models/subscription.models.js
+++ src/models/subscription.models.js
@@ -5,7 +5,12 @@ const subscriptionSchema = new Schema({
         // one who is subscribing
         type: Schema.Types.ObjectId,
         ref: "User"
-    }
+    },
+    channel: {
+        // one who is subscribing
+        type: Schema.Types.ObjectId,
+        ref: "User"
+    },
 }, {timestamps: true});
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema);
